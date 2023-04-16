561fd78bf6b9640cc9f655b0fc9c55ae55bc5f34
Bor Bálint <br.blnt10@gmail.com>
2023-04-16 18:19:35 +0200

    add service files
    - add service class

---
+++ hotel-booking/src/main/java/nostra/cosa/hotelbooking/service/exceptions/NotFoundException.java
@@ -0,0 +1,12 @@
+package nostra.cosa.hotelbooking.service.exceptions;
+
+public class NotFoundException extends Exception {
+
+  public NotFoundException() {
+  }
+
+  public NotFoundException(String message) {
+    super(message);
+  }
+
+}
---
+++ hotel-booking/src/main/java/nostra/cosa/hotelbooking/service/service/impl/AccommodationService.java
@@ -0,0 +1,36 @@
+package nostra.cosa.hotelbooking.service.service.impl;
+
+import java.util.List;
+
+import nostra.cosa.hotelbooking.service.dto.AccommodationDTO;
+import nostra.cosa.hotelbooking.service.service.ServiceInterface;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccommodationService implements ServiceInterface<AccommodationDTO> {
+
+  @Override
+  public List<AccommodationDTO> getAll() {
+    return null;
+  }
+
+  @Override
+  public AccommodationDTO getById(Long id) {
+    return null;
+  }
+
+  @Override
+  public AccommodationDTO update(AccommodationDTO newAccommodation) {
+    return null;
+  }
+
+  @Override
+  public AccommodationDTO create(AccommodationDTO newAccommodation) {
+    return null;
+  }
+
+  @Override
+  public void delete(Long id) {
+
+  }
+}
