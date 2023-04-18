e449e0ea255d00ee9b53ddc8904e82f68b45a61d
Négyesi Zoltán <zoltannegyesi4@gmail.com>
2023-04-18 23:33:44 +0200

    Auth configs

---
+++ hotel-booking/src/main/java/nostra/cosa/hotelbooking/config/MethodSecurityConfig.java
@@ -0,0 +1,20 @@
+package nostra.cosa.hotelbooking.config;
+
+import nostra.cosa.hotelbooking.auth.service.AuthorizationService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@Configuration
+@EnableMethodSecurity
+public class MethodSecurityConfig {
+
+    @Bean
+    static MethodSecurityExpressionHandler methodSecurityExpressionHandler() {
+        DefaultMethodSecurityExpressionHandler expressionHandler = new DefaultMethodSecurityExpressionHandler();
+        expressionHandler.setPermissionEvaluator(new AuthorizationService());
+        return expressionHandler;
+    }
+}
---
+++ hotel-booking/src/main/java/nostra/cosa/hotelbooking/config/WebConfig.java
@@ -0,0 +1,19 @@
+package nostra.cosa.hotelbooking.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${auth.origin.url}")
+    private String authOriginUrl;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/User/login").allowedOrigins(authOriginUrl).allowCredentials(true);
+        registry.addMapping("/User/register").allowedOrigins(authOriginUrl).allowCredentials(true);
+    }
+}
