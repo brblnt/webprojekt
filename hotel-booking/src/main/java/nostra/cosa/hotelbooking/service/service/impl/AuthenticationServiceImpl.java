91e02dad91d4df52dc37bf3dea8cfdc746dadc81
Bor Bálint <br.blnt10@gmail.com>
2023-05-07 17:03:14 +0200

    add service class
    - add new service class (auth/room)

--- hotel-booking/src/main/java/nostra/cosa/hotelbooking/service/service/impl/AuthenticationServiceImpl.java
+++ hotel-booking/src/main/java/nostra/cosa/hotelbooking/service/service/impl/AuthenticationServiceImpl.java
@@ -1,5 +1,48 @@
 package nostra.cosa.hotelbooking.service.service.impl;
 
-public class AuthenticationServiceImpl {
-  //TODO
+import java.util.List;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import nostra.cosa.hotelbooking.auth.dto.AuthenticationDataDTO;
+import nostra.cosa.hotelbooking.service.exceptions.NotFoundException;
+import nostra.cosa.hotelbooking.service.service.BookingService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Authentication Service class, implements ServiceInterface with AuthenticationDataDTOs.
+ */
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class AuthenticationServiceImpl implements BookingService<AuthenticationDataDTO> {
+
+  /**
+   * No implementation need.
+   */
+  @Override
+  public List<AuthenticationDataDTO> getAll() {
+    return null;
+  }
+
+  @Override
+  public AuthenticationDataDTO getById(Long id) throws NotFoundException {
+    return null;
+  }
+
+  @Override
+  public AuthenticationDataDTO update(AuthenticationDataDTO update) throws NotFoundException {
+    return null;
+  }
+
+  @Override
+  public AuthenticationDataDTO create(AuthenticationDataDTO create) {
+    return null;
+  }
+
+  @Override
+  public Boolean delete(Long id) {
+    return null;
+  }
+
 }
--- hotel-booking/src/main/java/nostra/cosa/hotelbooking/service/service/impl/RoomServiceImpl.java
+++ hotel-booking/src/main/java/nostra/cosa/hotelbooking/service/service/impl/RoomServiceImpl.java
@@ -1,5 +1,48 @@
 package nostra.cosa.hotelbooking.service.service.impl;
 
-public class RoomServiceImpl {
-  //TODO
+import java.util.List;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import nostra.cosa.hotelbooking.service.dto.RoomDTO;
+import nostra.cosa.hotelbooking.service.exceptions.NotFoundException;
+import nostra.cosa.hotelbooking.service.service.BookingService;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * Room Service class, implements ServiceInterface with RoomDTOs.
+ */
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class RoomServiceImpl implements BookingService<RoomDTO> {
+
+  /**
+   * No implementation need.
+   */
+  @Override
+  public List<RoomDTO> getAll() {
+    return null;
+  }
+
+  @Override
+  public RoomDTO getById(Long id) throws NotFoundException {
+    return null;
+  }
+
+  @Override
+  public RoomDTO update(RoomDTO update) throws NotFoundException {
+    return null;
+  }
+
+  @Override
+  public RoomDTO create(RoomDTO create) {
+    return null;
+  }
+
+  @Override
+  public Boolean delete(Long id) {
+    return null;
+  }
 }
