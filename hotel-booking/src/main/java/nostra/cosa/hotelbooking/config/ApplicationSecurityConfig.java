c539403db108d5c0da8eba4a6610ed52a4408f3a
Négyesi Zoltán <90441179+zoltannegyesi@users.noreply.github.com>
2023-05-28 15:20:36 +0200

    Update ApplicationSecurityConfig.java

--- hotel-booking/src/main/java/nostra/cosa/hotelbooking/config/ApplicationSecurityConfig.java
+++ hotel-booking/src/main/java/nostra/cosa/hotelbooking/config/ApplicationSecurityConfig.java
@@ -37,8 +37,8 @@ public class ApplicationSecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
-                .requestMatchers(URL_WHITELIST).authenticated()
-                .anyRequest().permitAll()  //TODO
+                .requestMatchers(URL_WHITELIST).permitAll()
+                .anyRequest().authenticated()
                 .and().csrf().disable()
                 .headers().frameOptions().disable()
                 .and()
