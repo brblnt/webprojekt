8bddf720ccaaf43552a55343e936c04cd6a02904
zoltannegyesi <zoltannegyesi4@gmail.com>
2023-05-31 14:56:11 +0200

    Refactored Image upload

---
+++ hotel-booking/src/main/java/nostra/cosa/hotelbooking/service/exceptions/ImageIOException.java
@@ -0,0 +1,10 @@
+package nostra.cosa.hotelbooking.service.exceptions;
+
+import java.io.IOException;
+
+public class ImageIOException extends IOException {
+
+    public ImageIOException(final String message) {
+        super(message);
+    }
+}
